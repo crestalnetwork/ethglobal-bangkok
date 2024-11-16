cc2ff11924a8d967bd290e7fdea6c24534e622fd
Brian Dhang <dangbh1002@gmail.com>
2024-11-17 04:50:17 +0700

    Add 1inch service

--- frontend/app/api/swap/route.js
+++ frontend/app/api/swap/route.js
@@ -1,15 +1,22 @@
 import { NextResponse } from "next/server";
 
-import { swap as coinbaseSwap, Coinbase } from '../../services/coinbase'
+// import { swap as oneInchSwap } from '../../services/1inch';
+import { swap as coinbaseSwap } from '../../services/coinbase';
 
 
 export async function POST(req) {
     const { amount } = await req.json();
 
     try {
+        // const trade = await oneInchSwap({
+        //     fromAssetId: 'usdc',
+        //     toAssetId: 'eth',
+        //     amount
+        // })
+
         const trade = await coinbaseSwap({
-            fromAssetId: Coinbase.assets.Usdc,
-            toAssetId: Coinbase.assets.Eth,
+            fromAssetId: 'usdc',
+            toAssetId: 'eth',
             amount
         })
 
