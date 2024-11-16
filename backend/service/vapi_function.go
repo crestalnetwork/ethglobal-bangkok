1dd1ed026026a357fd5608428bd9fbc4d40a843a
Muninn <ruihua@crestal.network>
2024-11-16 14:16:07 +0700

    feat: debug vapi function

--- backend/service/vapi_function.go
+++ backend/service/vapi_function.go
@@ -2,11 +2,13 @@ package service
 
 import (
 	"context"
+	"encoding/json"
 
 	"github.com/crestalnetwork/ethglobal-bangkok/backend/types"
 )
 
 func (s *Service) VAPIFunction(ctx context.Context, genericMessage map[string]interface{}) (*types.FunctionResult, error) {
-	s.log.Info("VAPIFunction called", "message", genericMessage)
-	return &types.FunctionResult{Result: "Hello, World!"}, nil
+	req, _ := json.Marshal(genericMessage)
+	s.log.Info("VAPIFunction called", "message", req)
+	return &types.FunctionResult{Result: "Trade accept!"}, nil
 }
