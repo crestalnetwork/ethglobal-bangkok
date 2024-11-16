63565ffc4e3ae13c65e9b3f92a7c2297cd93fb70
Brian Dhang <dangbh1002@gmail.com>
2024-11-17 01:02:42 +0700

    Update swap API

--- frontend/app/api/swap/route.js
+++ frontend/app/api/swap/route.js
@@ -15,32 +15,11 @@ export async function POST(req) {
     const { ethAmount } = await req.json();
 
     try {
-        console.log('POST ethAmount', ethAmount);
-
-        let wallet;
         const mySeed = fs.readFileSync(seedPath, 'utf-8');
-        console.log('mySeed', mySeed);
 
-        if (mySeed) {
-            let importedWallet = await Wallet.import(JSON.parse(mySeed));
-            wallet = importedWallet
-            console.log('Imported Wallet');
-        } else {
-            // Create a Wallet on BaseMainnet to trade assets with.
-            wallet = await Wallet.create({ networkId: Coinbase.networks.BaseMainnet });
-
-            // // Export the data required to re-instantiate the wallet. The data contains the seed and the ID of the wallet.
-            let seed = wallet.export();
-            let addressInfo = await wallet.getDefaultAddress();
-            const match = addressInfo.toString().match(/addressId:\s*'([^']+)'/);
-            const address = match ? match[1] : '';
-
-            const walletData = {
-                ...seed,
-                address,
-            }
-            fs.writeFileSync(seedPath, JSON.stringify(walletData, null, 4))
-        }
+        let importedWallet = await Wallet.import(JSON.parse(mySeed));
+        let wallet = importedWallet
+        console.log('Imported Wallet');
 
         // Fund the Wallet's default Address with ETH from an external source.
         // Trade ethAmount ETH to USDC.
